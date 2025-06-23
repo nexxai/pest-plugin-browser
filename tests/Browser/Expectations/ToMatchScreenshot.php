a0a800fae66fce5930b4db58946f6c5d5cc12532
Adrian Nürnberger <adrian@nuernberger.me>
2025-06-23 07:34:39 +0200

    fix screenshot testing, add option to switch slider mode

--- tests/Browser/Expectations/ToMatchScreenshot.php
+++ tests/Browser/Expectations/ToMatchScreenshot.php
@@ -2,8 +2,21 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\ExpectationFailedException;
+
 it('does match a screenshot', function (): void {
     $page = page()->goto('/');
 
     expect($page)->toMatchScreenshot();
 });
+
+it('does not match a screenshot', function (): void {
+    Route::get('screenshot-mismatch', function () {
+        return 'pest';
+    });
+
+    $page = page()->goto('screenshot-mismatch');
+
+    expect($page)->toMatchScreenshot(showDiff: true);
+})->throws(ExpectationFailedException::class);
