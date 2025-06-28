af796baf1f350215cf23939fdd541e7b9de556f2
Nuno Maduro <enunomaduro@gmail.com>
2025-06-28 17:53:59 +0100

    feat: adds configuration

--- src/Configuration.php
+++ src/Configuration.php
@@ -2,4 +2,45 @@
 
 declare(strict_types=1);
 
-//
+namespace Pest\Browser;
+
+use Pest\Browser\Enums\BrowserType;
+use Pest\Browser\Playwright\Playwright;
+
+/**
+ * @internal
+ *
+ * @codeCoverageIgnore
+ */
+final readonly class Configuration
+{
+    /**
+     * Defaults the browser to Chrome.
+     */
+    public function inChrome(): self
+    {
+        Playwright::setDefaultBrowserType(BrowserType::CHROME);
+
+        return $this;
+    }
+
+    /**
+     * Defaults the browser to Firefox.
+     */
+    public function inFirefox(): self
+    {
+        Playwright::setDefaultBrowserType(BrowserType::FIREFOX);
+
+        return $this;
+    }
+
+    /**
+     * Defaults the browser to Safari.
+     */
+    public function inSafari(): self
+    {
+        Playwright::setDefaultBrowserType(BrowserType::SAFARI);
+
+        return $this;
+    }
+}
