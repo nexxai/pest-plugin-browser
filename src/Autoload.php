dd1472b331cfd10703a3e66b3b45f6193b3b5af9
Nuno Maduro <enunomaduro@gmail.com>
2025-06-28 18:03:28 +0100

    chore: moves `visit` to the core

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,25 +2,7 @@
 
 declare(strict_types=1);
 
-use Pest\Browser\Api\ArrayablePendingAwaitablePage;
-use Pest\Browser\Api\PendingAwaitablePage;
 use Pest\Browser\Browsable;
 use Pest\Plugin;
 
 Plugin::uses(Browsable::class);
-
-if (! function_exists('visit')) {
-    /**
-     * Browse to the given URL.
-     *
-     * @template TUrl of array<int, string>|string
-     *
-     * @param  TUrl  $url
-     * @param  array<string, mixed>  $options
-     * @return (TUrl is array<int, string> ? ArrayablePendingAwaitablePage : PendingAwaitablePage)
-     */
-    function visit(array|string $url, array $options = []): ArrayablePendingAwaitablePage|PendingAwaitablePage
-    {
-        return test()->visit($url, $options);
-    }
-}
