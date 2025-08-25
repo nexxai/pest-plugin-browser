197998d8ed544a9ab95bb658ef255c743694f01d
ju5t <m@ju5t.nl>
2025-08-25 13:41:28 +0200

    tests: validate that hostname is changed

--- tests/Unit/Drivers/Laravel/LaravelHttpServerTest.php
+++ tests/Unit/Drivers/Laravel/LaravelHttpServerTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
+
 it('rewrites the URLs on JS files', function (): void {
     @file_put_contents(
         public_path('app.js'),
@@ -15,3 +17,14 @@ it('rewrites the URLs on JS files', function (): void {
     $page->assertSee('http://127.0.0.1')
         ->assertDontSee('http://localhost');
 });
+
+it('changes the hostname for all requests', function () {
+    Route::domain('pest.test')->group(function () {
+        Route::get('/about', fn (): string => 'Hello Pest');
+    });
+
+    pest()->browser()->withHostname('pest.test');
+
+    visit('/about')->assertSee('Hello Pest');
+});
+
