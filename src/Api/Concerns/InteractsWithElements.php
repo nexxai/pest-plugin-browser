45ea50014d35c931e6f2e30b9eae2be3c1c6b6e8
Jason McCreary <jason@pureconcepts.net>
2025-08-27 11:54:38 -0400

    Limit `press` to align with docs

--- src/Api/Concerns/InteractsWithElements.php
+++ src/Api/Concerns/InteractsWithElements.php
@@ -166,7 +166,17 @@ trait InteractsWithElements
      */
     public function press(string $button): Webpage
     {
-        $this->guessLocator($button)->click();
+        if (str_starts_with($button, '@')) {
+            $locator = $this->page->locator('button[data-test="'.mb_substr($button, 1).'"], input[type="button"][data-test="'.mb_substr($button, 1).'"]');
+        } else {
+            $locator = $this->page->getByRole('button', ['name' => $button]);
+
+            if ($locator->count() === 0) {
+                $locator = $this->page->locator('button[name="'.$button.'"], input[name="'.$button.'"]:is([type="button"], [type="submit"])');
+            }
+        }
+
+        $locator->click();
 
         return $this;
     }
