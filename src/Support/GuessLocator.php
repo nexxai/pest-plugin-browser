f362a75a3f3011dc5a932e578b8834addebe8cf1
Nuno Maduro <enunomaduro@gmail.com>
2025-09-09 21:13:53 +0100

    removes code

--- src/Support/GuessLocator.php
+++ src/Support/GuessLocator.php
@@ -70,6 +70,7 @@ final readonly class GuessLocator
 
         if ($this->scopeSelector !== null) {
             $scopedLocator = $this->page->locator($this->scopeSelector);
+
             return $this->page->unstrict(
                 fn (): Locator => $scopedLocator->getByText($selector, true),
             );
@@ -86,6 +87,6 @@ final readonly class GuessLocator
             return $selector;
         }
 
-        return $this->scopeSelector . ' >> ' . $selector;
+        return $this->scopeSelector.' >> '.$selector;
     }
 }
